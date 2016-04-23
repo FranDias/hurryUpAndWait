4b92632bcd9228f846272fbd320c97010651c7b0
Francisco Dias <frandias@gmail.com>
2016-04-23 17:54:50 -0400

    added some thoughts, starting to visualize this for an mvp

---
+++ ThinkingThroughLogic/Station.js
@@ -0,0 +1,34 @@
+var Station = function (name, line, direction, trainExits) {
+  this.name = name;
+  this.line = line;
+  // if we know start and end
+  // this doesn't really matter
+  // from a user perspective
+  // but I think I need it.
+  this.direction = 'aleweife';
+  // connection?
+
+  // function that says which
+}
+
+/*
+
+  Think of a station in terms of a train.
+
+
+  Mass ave:
+  this.direction = 'oak grove'
+  exits are (related to train):
+    train[0].front.left = ['elevator', 'escalator', 'stairs']
+    train[5].back.left = ['stairs']
+
+  exitsFromStation:
+    isHandicapAccessible
+    closestTrainExit
+
+
+  Ruggles:
+    train[4].back.left
+
+
+*/
---
+++ ThinkingThroughLogic/Train.js
@@ -0,0 +1,26 @@
+var Train = function() {
+  this.doors = {
+    front: {
+      left: ['elevator', 'escalator', 'stairs'],
+      right: '',
+    },
+    middle: {
+      left: '',
+      right: '',
+    },
+    back: {
+      left: '',
+      right: ''
+    }
+  }
+}
+
+
+/*
+
+  Red, Orange, Blue
+
+  6 doors, 2 sets of 3.
+  Train Position.
+
+*/
