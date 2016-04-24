957c9203d717e7066008cebfe6fd6e1d6ef2dea4
Francisco Dias <frandias@gmail.com>
2016-04-23 20:25:46 -0400

    more conjecture

--- ThinkingThroughLogic/Line.js
+++ ThinkingThroughLogic/Line.js
@@ -1,33 +1,4 @@
 var Line = function(color, arrayOfStations) {
   this.color = color;
-
 }
-
 var lines = ["Red", "Orange", "Green", "Blue"];
-
-/*
-
-Orange
-
-[
-'Oak Grove',
-'Malden Center',
-'Welington',
-'Assembly',
-'Sullivan Square',
-'Community College',
-'North station',
-'Haymarket',
-'State',
-'Downtown Crossing',
-'Chinatown',
-'Tufts Medical Center',
-'Back Bay',
-'Massachusetts Avenue',
-'Ruggles',
-'Roxbury Crossing',
-'Jackson Square',
-'Stony Brook',
-'Green Street',
-'Forest HIlls'
-]
---
+++ ThinkingThroughLogic/exit.js
@@ -0,0 +1,7 @@
+var Exit = function() {
+  this.access = ['stairs', 'escalator', 'elevator', 'ramp', 'flat'];
+  this.trainSide = ['left', 'right'];
+  this.trainCar = [0...5];
+  this.trainDoor = [0...2];
+
+}
